22d8c51fa00aff9778ab93fc54bb8ebc71858921
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-02-06 11:50:16 +0530

    Added push_to_github.sh to automate git commands.

---
+++ push_to_github.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Get file name
+full_name=$1
+file_name=`echo $full_name | cut -d "/" -f2`
+c_message=$2
+
+if [ -e $full_name ] 
+then
+    # Add file with git
+    git add $full_name
+
+    # Commit the file
+    if [ $c_message ]
+    then
+        `git commit -m "${c_message}"`
+    else
+        `git commit -m "Added ${file_name} assignment."`
+    fi
+    
+    # push to github
+    `git push origin main`
+
+    echo "Your file $file_name pushed to github."
+else
+    echo "Your entered file does not exists."
+fi
